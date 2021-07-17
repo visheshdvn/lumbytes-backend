e0a0a5033222394454ad228e273488de138b18ee
visheshdvn <vishesh.dh1@gmail.com>
2021-07-18 00:46:34 +0530

    url in production server added

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: 'https://admin.lumbytes.com',
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "f23231c2c32fa42a5e136f93ad873fef"),
